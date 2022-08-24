e4a7ccc098c75c9d739504f906bda7091bdfa547
Thiago Sabatucci <thisab@gmail.com>
2022-08-24 19:09:17 -0300

    code skeleton update for sqltore_test.go

--- pkg/server/datastore/sqlstore/sqlstore_test.go
+++ pkg/server/datastore/sqlstore/sqlstore_test.go
@@ -4,40 +4,6 @@ import (
 	"testing"
 )
 
-/*
-type Dialector struct {
-	DriverName string
-	DSN        string
-	sqlite.Dialector
-	Conn gorm.ConnPool
-}
-
-func (dialector Dialector) Initialize(db *gorm.DB) (err error) {
-	if dialector.DriverName == "" {
-		dialector.DriverName = sqlite.DriverName
-	}
-
-	if dialector.Conn != nil {
-		db.ConnPool = dialector.Conn
-	} else {
-		conn, err := sql.Open(dialector.DriverName, dialector.DSN)
-		if err != nil {
-			return err
-		}
-		db.ConnPool = conn
-	}
-
-	callbacks.RegisterDefaultCallbacks(db, &callbacks.Config{
-		LastInsertIDReversed: true,
-	})
-
-	for k, v := range dialector.ClauseBuilders() {
-		db.ClauseBuilders[k] = v
-	}
-
-	return
-}
-*/
 func TestCreateOrganization(t *testing.T) {
 	// TODO integration testing
 }
